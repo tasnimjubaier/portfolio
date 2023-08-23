d32199486826e5a650f95e1e2f4653e474a506cc
TJubaier <tasnim.jubaier@tigerit.com>
2023-08-23 16:47:45 +0600

    add three.js initials

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 
 import './App.css';
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { motion } from "framer-motion";
 
 import Home from './pages/Home';
 
 function App() {
+
+  const sceneRef = useRef(null);
+
+  useEffect(() => {
+    // Create a scene, camera, and renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.9, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    sceneRef.current.appendChild(renderer.domElement);
+
+    // Create a cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // Position the camera
+    camera.position.z = 5;
+
+    // Animation loop
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    return () => {
+      // Clean up the scene
+      sceneRef.current.removeChild(renderer.domElement);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Home />
-      asdf
-      <motion.div className='box' animate={{ x: 0 }}>
+      <div className='scene'>
+        <div ref={sceneRef}></div>
+      </div>
+      {/* <motion.div className='box' animate={{ x: 0 }}>
         asdffe
-      </motion.div>
+      </motion.div> */}
     </div>
   );
 }
